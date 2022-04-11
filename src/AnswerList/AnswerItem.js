8c618e11a71f38c68bf42d6722892d7c62cc16c8
Shahriyar <shakompk@gmail.com>
2022-04-11 14:26:18 +0400

    add logic

--- src/AnswerList/AnswerItem.js
+++ src/AnswerList/AnswerItem.js
@@ -1,7 +1,10 @@
 function AnswerItem(props) {
-  return (
-    <li onClick={() => props.onAnswerClick(props.chosenId)}>{props.children}</li>
-  );
+    return (
+      //onAnswerClick ты посылал id а по сути тебе надо послать весь обьект который содержит ответ
+        <li key={props.key} onClick={() => props.onAnswerClick(props)}>
+            {props.children}
+        </li>
+    );
 }
 
 export default AnswerItem;
--- src/containers/ActiveQuiz.js
+++ src/containers/ActiveQuiz.js
@@ -1,22 +1,22 @@
 import AnswerLists from "../AnswerList/AnswerList";
 
 function ActiveQuiz(props) {
-  return (
-    <div className="QuizWrapper">
-      <div className="QuizQuestion">
-        <p>
-          {props.questionIndex}.{props.quiz.question}
-        </p>
-        <p>
-          {props.questionIndex} / {props.quizLength}
-        </p>
-      </div>
-      <AnswerLists
-        answers={props.quiz.answers}
-        onAnswerClick={props.onAnswerClick}
-      />
-    </div>
-  );
+    return (
+        <div className="QuizWrapper">
+            <div className="QuizQuestion">
+                <p>
+                    {props.questionIndex}.{props.quiz.question}
+                </p>
+                <p>
+                    {props.questionIndex} / {props.quizLength}
+                </p>
+            </div>
+            <AnswerLists
+                answers={props.quiz.answers}
+                onAnswerClick={props.onAnswerClick}
+            />
+        </div>
+    );
 }
 
 export default ActiveQuiz;
